348e219cb45e79ca7ae53ae724be168c7b3632c6
dianakoliada <koliada.di@gmail.com>
2025-12-12 20:03:02 +0200

    made the fifth task

--- Program.cs
+++ Program.cs
@@ -60,6 +60,7 @@ for (int i = 0; i <= row; i++)
 Просте число - це число, яке ділиться лише на 1 і на себе.
 */
 
+/*
 Console.WriteLine("Enter a number:");
 int number = Convert.ToInt32(Console.ReadLine());
 
@@ -80,5 +81,53 @@ for (int i = 2; i <= number; i++)
     if (isPrime) Console.WriteLine(i);
 
 }
+*/
+
+
+// 5
+/*
+Калькулятор оплати праці за годину: Напишіть "програму, яка запитує в користувача кількість годин, працюваних за день, та розмір годинної ставки. 
+Після введення значень програма має обчислити та вивести оплату за день.
+*/
+
+bool isLastEmployee = false;
+var userInput = "";
+bool isProperHoursInput = false;
+bool isProperSalaryInput = false;
+double hours = 0;
+decimal salaryPerHour = 0;
+decimal salaryTotal = 0;
+
+do
+{
+    do
+    {
+        Console.WriteLine("Enter your working hours:");
+        var userInputHours = Console.ReadLine();
+        isProperHoursInput = double.TryParse(userInputHours, out hours);
+    } while (!isProperHoursInput);
+
+    do
+    {
+        Console.WriteLine("Enter your salary per hour:");
+        var userInputSalary = Console.ReadLine();
+        isProperSalaryInput = decimal.TryParse(userInputSalary, out salaryPerHour);
+    } while (!isProperSalaryInput);
+
+    salaryTotal = (decimal)hours * salaryPerHour;
+    Console.WriteLine($"Congrats! Today you have earned {salaryTotal} UAH.");
+
+    Console.WriteLine("Press any key.");
+    userInput = Console.ReadLine();
+    if (userInput == "-")
+    {
+        isLastEmployee = true;
+        Console.WriteLine("That was the last employee.");
+    }
+
+} while (!isLastEmployee);
+
+
+
 
 
